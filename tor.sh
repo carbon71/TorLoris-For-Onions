3f0fe7e23b270da5c9d7b529e5e46ea39a508e98
carb0ni <54814198+carb0ni@users.noreply.github.com>
2019-09-03 01:40:54 +0800

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -1,11 +1,9 @@
 if [[ ! -d multitor ]]; then 
 mkdir multitor;
 fi
-default_ins="1"
-inst="${inst:-${default_inst}}"
 
 let i=1
-while [[ $i -le $inst ]]; do
+while [[ $i -le 9 ]]; do
 port=$((9050+$i))
 printf "SOCKSPort %s\nDataDirectory /var/lib/tor%s" $port $i > multitor/multitor$i 
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Starting Tor on port:\e[0m\e[1;77m 905%s\e[0m\n" $i 
